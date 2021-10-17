e01667c3941d95cf53974d458113b9bb2989513a
ray0780 <t110360206@ntut.org.tw>
2021-10-17 16:54:18 +0800

    finish

---
+++ 2.25/source/main.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void)
+{
+	printf("PPPPPPPPP\n\
+    P   P\n\
+    P   P\n\
+    P   P\n\
+     P P \n\
+\n\
+  JJ\n\
+ J\n\
+J\n\
+ J\n\
+  JJJJJJJ\n\
+\n\
+DDDDDDDDD\n\
+D       D\n\
+D       D\n\
+ D     D\n\
+  DDDDD\n");
+	system("pause");
+	return 0;
+
+}
