6a3c27394128dab235da5de5fdb83f7a59a47f48
ray0780 <t110360206@ntut.org.tw>
2021-10-17 17:11:16 +0800

    finish

---
+++ 2.26/source/main.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void)
+{
+	int a;
+	int b;
+	printf(" Enter two integers:");
+	scanf_s("%d%d", &a, &b);
+	if (a % b == 0)
+	{
+		printf(" %d is a multiple of %d\n", a, b);
+	}
+	else
+	{
+		printf(" %d is not a multiple of %d\n", a, b);
+	}
+	system("pause");
+	return 0;
+}
