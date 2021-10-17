ffa58f1a3e0e61e582ebf8e6de143460606a70bf
ray0780 <t110360206@ntut.org.tw>
2021-10-17 17:35:29 +0800

    finish

---
+++ 2.27/source/main.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void)
+{
+	printf("\t    *\n");
+	printf("\t   ***\n");
+	printf("\t  *****\n");
+	printf("\t *******\n");
+	printf("\t*********\n");
+	printf("\n\
+\n");
+	printf("\t    *\n\
+\t   ***\n\
+\t  *****\n\
+\t *******\n\
+\t*********\n");
+	system("pause");
+	return 0;
+}
